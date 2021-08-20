71a80528a157159bf4c068c9d38135b7c300ef4e
RishitaMitra <mitrarishita4@gmail.com>
2021-08-20 19:46:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import './Components/header'
-import './Components/style.css'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//import Home from "./components/LandingPage/Home";
+//import Login from "./components/Login/Login";
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+//import "./Components/style.css";
+
 
 function App() {
-  return (
+	return (
     <>
-    <header/>
-    <style/>
+		<Router>
+			<Navbar />
+			
+			<Switch>
+				<Route exact path='/' children={<Header/>} />
+				
+        		
+        
+			</Switch>
+		</Router>
     </>
-  );
+	);
 }
 
 export default App;
--- src/Components/header.js
+++ src/Components/header.js
@@ -1,46 +1,25 @@
-import React from 'react'
+import React from "react";
 //import Media from 'react-media'
-import './picture/human.png'
-import './picture/lap.png'
-import $ from 'jquery'
+import firstpic from "./picture/human.png";
+import secondpic from "./picture/lap.png";
+import "./style.css";
+
 export default function Header(){
     return(
         <>
         <head>
          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous"/>
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <script>
-    $(document).ready(function(){
-        $('.fa-bars').click(function(){
-            $('.nav-list').toggleClass('show');
-        })
-    });
     
-    </script>
+   
     </head>
 <body>
     <div className="contain">
-    <nav className="navbar">
-        <i className="fa fa-bars" aria-hidden="true" id="check"></i>
-       <b><h1>BLOG</h1></b>
-       <div className="new">
-           
-        <ul className="nav-list" id="menulist">
-                
-            <div className="nav-list-li">
-                <li><a href="#home">HOME</a></li>
-                <li><a href="#about">ABOUT</a></li>
-                <li><a href="#services">SERVICES</a></li>
-                <li><a href="#contact">LOG IN/SIGN UP</a></li>
-            </div>
-        </ul>
-        </div>
-    </nav>
+    
     <div className="contain-img">
     
        
        <div className="image">
-       <img src={"picture/human.png"} alt="pic"/></div>
+       <img src={firstpic} alt=" "/></div>
        <div className="main">
        <div className="big">We Share</div><br/>
        <div className="blue">Knowledge</div><br/>
@@ -50,10 +29,11 @@ export default function Header(){
         <button className="btn">Start A Blog</button></div><br/>
         </div>
     </div>
-    </div>
+    </div><br/><br/><br/><br/>
+    <div className="parent">
     <div className="two">
     <div className="second">
-       <img src={"picture/lap.png"} alt="pic2"/>
+       <img src={secondpic} alt=" "/>
     </div>
     <div className="second-main">
     <div className="create">
@@ -65,8 +45,25 @@ export default function Header(){
     <div className="button">
         <button className="btn">Start A Blog</button></div>
         </div>
+        </div></div>
+        <br/><br/><br/>
+        <div className="parent">
+        <div className="two">
+    <div className="second">
+       <img src={secondpic} alt=" "/>
+    </div>
+    <div className="second-main">
+    <div className="create">
+        Content<br/>Design
+    </div><br/>
+    <div className="small">
+        Learn from the best.
+    </div><br/>
+    <div className="button">
+        <button className="btn">Start A Blog</button></div>
         </div>
-    
+        </div></div>
+           
             </body>
 
         </>
